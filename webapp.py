886fe6934b0730564302f7eeff84c09597460e78
Neta <netarvd@gmail.com>
2016-07-21 14:39:57 +0300

    changes to webapp

--- webapp.py
+++ webapp.py
@@ -2,12 +2,9 @@ from flask import Flask, render_template, request
 from flask.ext.wtf import Form
 from flask.ext.sqlalchemy import SQLAlchemy
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = \
-	'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SECRET_KEY'] = 'guess who'
 
 db = SQLAlchemy(app)
@@ -16,13 +13,10 @@ db = SQLAlchemy(app)
 def entry():
 	return render_template('entry.html')
 
-class SignForm(Form):
-	name = StringField('what is your')
-
-
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
 	if request.method == 'POST':
+		print(request.form['firstname'])
 		return redirect(url_for('home'))
 	return render_template('signup.html')
 
@@ -41,6 +35,7 @@ def home():
 @app.route ('/canvas/user/<name>')
 def canvas(name):
 	return render_template('canvas.html', name=name)
+
 @app.route ('/chat/user/<name>')
 def chat(name):
 	return render_template('chat.html')
