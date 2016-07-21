c991519e29f3af06657f6e6d1ca40914338ebf62
Berge Hagopian <bergehagopian1@hotmail.com>
2016-07-21 11:19:34 +0300

    Connecting entry to sign up and log in

--- webapp.py
+++ webapp.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def entry():
 	return render_template('entry.html')
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
+	if request.method == 'POST':
+		return redirect(url_for('home'))
 	return render_template('signup.html')
 
 @app.route('/login')
@@ -16,6 +18,22 @@ def login():
 @app.route('/user/<name>')
 def profile(name):
 	return render_template('profile.html', name = name)
+
+@app.route('/home/user/<name>')
+def home():
+	return render_template('home.html', name=name)
+
+@app.route ('/canvas/user/<name>')
+def canvas(name):
+	return render_template('canvas.html', name=name)
+@app.route ('/chat/user/<name>')
+def chat(name):
+	return render_template('chat.html')
+
+
+
+
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
