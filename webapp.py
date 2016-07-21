af1ff3c39b0e0680827cf33bbd66288d58a5bc37
Neta <netarvd@gmail.com>
2016-07-21 12:31:15 +0300

    edited signup and webapp, imported SQLAlchemy

--- webapp.py
+++ webapp.py
@@ -1,9 +1,24 @@
 from flask import Flask, render_template, request
+from flask.ext.wtf import Form
+from flask.ext.sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+	'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SECRET_KEY'] = 'guess who'
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def entry():
 	return render_template('entry.html')
+
+class SignForm(Form):
+	name = StringField('what is your')
+
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
