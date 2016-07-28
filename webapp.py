090ef4daddd3708938b54c2eb0b95a3a53e2975e
Berge Hagopian <berge16@meet.mit.edu>
2016-07-28 11:48:29 +0300

    fix navbar

--- webapp.py
+++ webapp.py
@@ -143,6 +143,10 @@ def chat(name):
 def about():
 	return render_template('about.html')
 
+@app.route ('/contact')
+def contact():
+	return render_template('contact.html')
+
 @app.route('/profile')
 def uploads():
     posts = [
