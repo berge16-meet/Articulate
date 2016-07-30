cad70aebf701618f26e3bef3cfe8392e2258c196
Nathaniel Navarro <nathaniel16@meet.mit.edu>
2016-07-30 13:38:20 +0300

    fixed erros from morning, still working on upload

--- database.py
+++ database.py
@@ -8,68 +8,68 @@ from sqlalchemy.orm import relationship
 Base = declarative_base()
 
 class User(Base):
-	__tablename__ = 'user'
-	id = Column(Integer, primary_key = True)
-	firstname = Column(String(64))
-	lastname = Column(String(64))
-	email = Column(String(64),unique=True)
-	username = Column(String(64),unique=True)
-	password = Column(String(64))
-	nationality = Column(String(64))
-	gender = Column(String(20))
-	date = Column(String(64))
-	bio  = Column(String(250))
-	profilepic = Column(String(250))
-	photos = relationship('Gallery', backref = 'User', lazy = 'dynamic')
+  __tablename__ = 'user'
+  id = Column(Integer, primary_key = True)
+  firstname = Column(String(64))
+  lastname = Column(String(64))
+  email = Column(String(64),unique=True)
+  username = Column(String(64),unique=True)
+  password = Column(String(64))
+  nationality = Column(String(64))
+  gender = Column(String(20))
+  date = Column(String(64))
+  bio  = Column(String(250))
+  profilepic = Column(String(250))
+  photos = relationship('Gallery', backref = 'User', lazy = 'dynamic')
 
 '''
 topic_gallery_association_table = Table('association', Base.metadata, Column('topic_id', Integer, ForeignKey('topic.id')), Column('gallery_id', Integer, ForeignKey('gallery.id')))
 
 class Topic(Base):
-	__tablename__ = 'topic'
-	id = Column(Integer, primary_key=True)
-	topic = Column(String(64), unique=True)
-	posts = relationship("Gallery", secondary = topic_gallery_association_table, back_populates="topics")
+  __tablename__ = 'topic'
+  id = Column(Integer, primary_key=True)
+  topic = Column(String(64), unique=True)
+  posts = relationship("Gallery", secondary = topic_gallery_association_table, back_populates="topics")
 '''
 
-	#posts = relationship("Gallery", secondary = topic_gallery_association_table, backref="topics")
+  #posts = relationship("Gallery", secondary = topic_gallery_association_table, backref="topics")
 
 class Gallery(Base):
-	__tablename__ = 'gallery'
-	id = Column(Integer, primary_key = True)
-	user_id = Column(Integer, ForeignKey('user.id'))
-	photo = Column(String(255))
-	description = Column(String(140))
-	likes = Column(Integer)
-	#time = Column(Time)
-	#topics = relationship("Topics", secondary = topic_gallery_association_table,  back_populates="galleries")
-	comments = relationship('Comment', backref='Gallery')
+  __tablename__ = 'gallery'
+  id = Column(Integer, primary_key = True)
+  user_id = Column(Integer, ForeignKey('user.id'))
+  file_path = Column(String(255))
+  description = Column(String(140))
+  likes = Column(Integer)
+  #time = Column(Time)
+  #topics = relationship("Topics", secondary = topic_gallery_association_table,  back_populates="galleries")
+  comments = relationship('Comment', backref='Gallery')
 
 class Comment(Base):
-	__tablename__='comments'
-	id = Column(Integer,primary_key=True)
-	gallery_id = Column(Integer, ForeignKey('gallery.id'))
-	#parent_id = Column(Integer, ForeignKey('comment.id'))
-	user_id = Column(Integer)
-	text = Column(String(400))
-	time=Column(Time)
-	#sub_comments = relationship('Comment', backref='Comment')
+  __tablename__='comments'
+  id = Column(Integer,primary_key=True)
+  gallery_id = Column(Integer, ForeignKey('gallery.id'))
+  #parent_id = Column(Integer, ForeignKey('comment.id'))
+  user_id = Column(Integer)
+  text = Column(String(400))
+  time=Column(Time)
+  #sub_comments = relationship('Comment', backref='Comment')
 
 # class Comment(Base):
-# 	__tablename__='comments'
-# 	id = Column(Integer,primary_key=True)
-# 	gallery_id = Column(Integer, ForeignKey('gallery.id'))
-# 	parent_id = Column(Integer, ForeignKey('comment.id'))
-# 	user_id = Column(Integer)
-# 	text = Column(String(400))
-# 	time=Column(Time)
-# 	sub_comments = relationship('Comment', backref='Comment')
+#   __tablename__='comments'
+#   id = Column(Integer,primary_key=True)
+#   gallery_id = Column(Integer, ForeignKey('gallery.id'))
+#   parent_id = Column(Integer, ForeignKey('comment.id'))
+#   user_id = Column(Integer)
+#   text = Column(String(400))
+#   time=Column(Time)
+#   sub_comments = relationship('Comment', backref='Comment')
 
 
 #def foo(comments):
-#	output = []
-#	for comment in comments:
-#		output.append(comment)
-#		for sub_comment in comment.sub_comments:
-#			output += foo(sub_comment)
-#		return output
+# output = []
+# for comment in comments:
+#   output.append(comment)
+#   for sub_comment in comment.sub_comments:
+#     output += foo(sub_comment)
+#   return output
