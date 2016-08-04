069dd2504e85318d0f6d50e2bcab69e93f69b103
Berge Hagopian <berge16@meet.mit.edu>
2016-08-05 02:28:01 +0300

    almost done with navbars

--- webapp.py
+++ webapp.py
@@ -169,6 +169,10 @@ def religion():
 def about():
   return render_template('about.html')
 
+@app.route ('/activeabout')
+def activeabout():
+  return render_template('activeabout.html')
+
 @app.route ('/contact')
 def contact():
   return render_template('contact.html')
